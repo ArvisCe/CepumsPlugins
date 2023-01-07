17a783769ab0b03247136932ed698024a1c7d868
Arvis Ceirulis <103632556+ArvisCe@users.noreply.github.com>
2023-01-07 09:18:10 +0200

    Add files via upload
    - pievienota pieeja komandām
    - izveidota plugina konfigurācija

--- Cepums/src/main/java/me/arvis/cepums/cepums/Cepums.java
+++ Cepums/src/main/java/me/arvis/cepums/cepums/Cepums.java
@@ -1,14 +1,21 @@
 package me.arvis.cepums.cepums;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Cepums extends JavaPlugin {
+import java.util.List;
 
+public final class Cepums extends JavaPlugin {
+    FileConfiguration config;
+    public String GetCookieMessage = "";
     @Override
     public void onEnable() {
         // Plugin startup logic
+    saveDefaultConfig();
+    config = getConfig();
+    getLogger().info("Plugin ieslēdzas");
     getCommand("Ctests").setExecutor(new commands());
     getCommand("Cversija").setExecutor(new commands());
     getCommand("cepums").setExecutor(new commands());
--- Cepums/src/main/java/me/arvis/cepums/cepums/commands.java
+++ Cepums/src/main/java/me/arvis/cepums/cepums/commands.java
@@ -6,8 +6,13 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.Time;
 import java.text.SimpleDateFormat;
@@ -16,27 +21,36 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.*;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import static org.bukkit.Bukkit.getLogger;
+
 public class commands implements CommandExecutor{
     List CookieCommandP = new ArrayList<String>();
     List CookieCommandT = new ArrayList<Long>();
     List MilkCommandP = new ArrayList<String>();
     List MilkCommandT = new ArrayList<Long>();
 
+    private static final Cepums plugin = (Cepums) Bukkit.getPluginManager().getPlugin("Cepums");
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
         if(command.getName().equalsIgnoreCase("Ctests"))
         {
-            player.sendMessage(ChatColor.GREEN + "komanda strādā!");
+            player.sendMessage(ChatColor.GREEN + "plugins strādā!");
         }
         if(command.getName().equalsIgnoreCase("Cversija"))
         {
-            player.sendMessage(ChatColor.GREEN + "Cepuma plugin versija: " + ChatColor.BLUE + "v1.1");
+            player.sendMessage(ChatColor.GREEN + "Version: " + ChatColor.BLUE + "v2.5");
         }
+
+
         //CEPUMS
+
+
         if(command.getName().equalsIgnoreCase("cepums"))
         {
-            long CoolDown =  30000;
+            long CoolDown =  plugin.getConfig().getLong("CepumaLaiks");
             if(CookieCommandP.contains(player))
             {
                 int index = CookieCommandP.indexOf(player);
@@ -45,25 +59,28 @@ public class commands implements CommandExecutor{
                 long time = (long) CookieCommandT.get(index);
                 if(  ((time-currentTime)/1000)+CoolDown/1000 <= 0   )
                 {
-                    player.sendMessage(ChatColor.GREEN + "Ņem savu cepumu, "+player.getName()+"!");
+                    player.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("SanemCepumuZina").replace("%speletajs%", player.getName()));
                     CookieCommandT.add(currentTime);
                     CookieCommandP.add(player);
                     CookieCommandT.remove(index);
                     CookieCommandP.remove(index);
                     ItemStack cookies = new ItemStack(Material.COOKIE);
-                    cookies.setAmount(1);
+                    cookies.setAmount(plugin.getConfig().getInt("CepumuDaudzums"));
                     player.getInventory().addItem(cookies);
                 }
                 else
                 {
-                    player.sendMessage(ChatColor.RED + "Dabūsī vēlvienu cepumu pēc "+(((time-currentTime)/1000)+CoolDown/1000)+" sekundēm!");
+                    String timeStr = "" + (((time-currentTime)/1000)+CoolDown/1000);
+                    player.sendMessage(ChatColor.RED + plugin.getConfig().getString("CepumaLaikaZina")
+                            .replace("%laiks%", timeStr)
+                            .replace("%speletajs%", player.getName()));
                 }
             }
             else
             {
-                player.sendMessage(ChatColor.GREEN + "Ņem savu cepumu, "+player.getName()+"!");
+                player.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("SanemCepumuZina").replace("%speletajs%", player.getName()));
                 ItemStack cookies = new ItemStack(Material.COOKIE);
-                cookies.setAmount(1);
+                cookies.setAmount(plugin.getConfig().getInt("CepumuDaudzums"));
                 player.getInventory().addItem(cookies);
                 CookieCommandP.add(player);
 
@@ -73,10 +90,14 @@ public class commands implements CommandExecutor{
             }
 
         }
+
+
         //PIENS
+
+
         if(command.getName().equalsIgnoreCase("piens"))
         {
-            long CoolDown =  30000;
+            long CoolDown =  plugin.getConfig().getLong("PienaLaiks");
             if(MilkCommandP.contains(player))
             {
                 int index = MilkCommandP.indexOf(player);
@@ -85,26 +106,29 @@ public class commands implements CommandExecutor{
                 long time = (long) MilkCommandT.get(index);
                 if(  ((time-currentTime)/1000)+CoolDown/1000 <= 0   )
                 {
-                    player.sendMessage(ChatColor.GREEN + "Ņem savu pienu, "+player.getName()+"!");
+                    player.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("SanemPienuZina").replace("%speletajs%", player.getName()));
                     MilkCommandT.add(currentTime);
                     MilkCommandP.add(player);
                     MilkCommandT.remove(index);
                     MilkCommandP.remove(index);
-                    ItemStack cookies = new ItemStack(Material.MILK_BUCKET);
-                    cookies.setAmount(1);
-                    player.getInventory().addItem(cookies);
+                    ItemStack milk = new ItemStack(Material.MILK_BUCKET);
+                    milk.setAmount(plugin.getConfig().getInt("PienaDaudzums"));
+                    player.getInventory().addItem(milk);
                 }
                 else
                 {
-                    player.sendMessage(ChatColor.RED + "Dabūsī vēl pienu pēc "+(((time-currentTime)/1000)+CoolDown/1000)+" sekundēm!");
+                    String timeStr = "" + (((time-currentTime)/1000)+CoolDown/1000);
+                    player.sendMessage(ChatColor.RED + plugin.getConfig().getString("PienaLaikaZina")
+                            .replace("%laiks%", timeStr)
+                            .replace("%speletajs%", player.getName()));
                 }
             }
             else
             {
-                player.sendMessage(ChatColor.GREEN + "Ņem savu pienu, "+player.getName()+"!");
-                ItemStack cookies = new ItemStack(Material.MILK_BUCKET);
-                cookies.setAmount(1);
-                player.getInventory().addItem(cookies);
+                player.sendMessage(ChatColor.GREEN + plugin.getConfig().getString("SanemPienuZina").replace("%speletajs%", player.getName()));
+                ItemStack milk = new ItemStack(Material.MILK_BUCKET);
+                milk.setAmount(plugin.getConfig().getInt("PienaDaudzums"));
+                player.getInventory().addItem(milk);
                 MilkCommandP.add(player);
 
                 Date date = new Date();
